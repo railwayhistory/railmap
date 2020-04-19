7ab84c2360aaf4918357ba7cc15d7786620f335e
Martin Hoffmann <hn@nvnc.de>
2020-04-19 17:12:38 +0200

    More symbols.

--- src/library/symbols.rs
+++ src/library/symbols.rs
@@ -1,5 +1,6 @@
 //! All the available symbols.
 
+use std::f64::consts::PI;
 use crate::canvas::Canvas;
 use crate::import::units;
 
@@ -8,10 +9,21 @@ const SYMBOLS: &[(&'static str, &'static dyn Fn(&Canvas))] = &[
     ("de_bf", &|canvas| {
         let sw = SW * canvas.canvas_bp();
         let sh = SH * canvas.canvas_bp();
+        let ds = DS * canvas.canvas_bp();
 
         canvas.move_to(-0.5 * sw, 0.);
-        canvas.line_to(-0.5 * sw, sh);
-        canvas.line_to(0.5 * sw, sh);
+        canvas.line_to(-0.5 * sw, sh - ds);
+        canvas.curve_to(
+            -0.5 * sw, sh - 1.5 * ds,
+            -0.5 * sw + 0.5 * ds, sh,
+            -0.5 * sw + ds, sh
+        );
+        canvas.line_to(0.5 * sw - ds, sh);
+        canvas.curve_to(
+            0.5 * sw - 0.5 * ds, sh,
+            0.5 * sw, sh - 0.5 * ds,
+            0.5 * sw, sh - ds
+        );
         canvas.line_to(0.5 * sw, 0.);
         canvas.close_path();
         canvas.fill();
@@ -27,6 +39,31 @@ const SYMBOLS: &[(&'static str, &'static dyn Fn(&Canvas))] = &[
         canvas.line_to(0.5 * sw - 0.5 * sp, sh - 0.5 * sp);
         canvas.line_to(0.5 * sw - 0.5 * sp, 0.);
         canvas.set_line_width(sp);
+        canvas.stroke();
+    }),
+
+    ("de_dirgr", &|canvas| {
+        let sp = SP * canvas.canvas_bp();
+        let dt = DT * canvas.canvas_bp();
+
+        let u = 0.8;
+        canvas.set_line_width(sp);
+        canvas.move_to(0., 0.);
+        canvas.line_to(0., 2. * u * dt);
+        canvas.stroke();
+        canvas.arc(0., 3. * u * dt, u * dt, 0., 2. * PI);
+        canvas.stroke();
+        canvas.arc(0., 3. * u * dt, 0.5 * u * dt, 0., 2. * PI);
+        canvas.fill();
+    }),
+
+    ("statdt", &|canvas| {
+        let sp = SP * canvas.canvas_bp();
+        let dt = DT * canvas.canvas_bp();
+
+        canvas.set_line_width(sp);
+        canvas.move_to(0., 0.);
+        canvas.line_to(0., dt);
         canvas.stroke();
     }),
 ];
@@ -53,7 +90,8 @@ impl Symbol {
 }
 
 
+const DT: f64 = units::DT;
 const SW: f64 = 2.25 * units::MM;
 const SH: f64 = 2.1 * units::MM;
 const SP: f64 = 0.5 * units::PT;
-
+const DS: f64 = 0.2 * units::PT;
