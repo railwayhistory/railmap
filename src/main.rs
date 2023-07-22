aa21cd29f803d27a942b5e0162802fd4273c2357
Martin Hoffmann <hn@nvnc.de>
2023-07-22 16:43:25 +0200

    Move to clap 4.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::time::Instant;
-use clap::{Arg, ArgMatches, Command, crate_version, crate_authors};
+use clap::{Arg, ArgAction, ArgMatches, Command, crate_version, crate_authors};
 use railmap::{Config, LoadFeatures, Server};
 use railmap::theme::Theme;
 
@@ -10,7 +10,7 @@ async fn run<T: Theme>(config: Config, matches: ArgMatches, mut theme: T) {
     let start = Instant::now();
     theme.config(&config);
     let mut features = LoadFeatures::new(&theme);
-    match matches.values_of("region") {
+    match matches.get_many::<String>("region") {
         Some(values) => {
             let mut values: Vec<_> = values.collect();
             values.sort();
@@ -42,7 +42,7 @@ async fn run<T: Theme>(config: Config, matches: ArgMatches, mut theme: T) {
         }
     };
 
-    let addr_str = matches.value_of("listen").unwrap();
+    let addr_str: &String = matches.get_one("listen").unwrap();
     let addr = match SocketAddr::from_str(addr_str) {
         Ok(addr) => addr,
         Err(_) => {
@@ -53,7 +53,7 @@ async fn run<T: Theme>(config: Config, matches: ArgMatches, mut theme: T) {
 
     let server = Server::new(
         theme, features,
-        matches.value_of("style").unwrap().into(),
+        matches.get_one::<String>("style").unwrap().clone(),
     );
     eprintln!("Server ready after {:.03}s.", start.elapsed().as_secs_f32());
     server.run(addr).await;
@@ -70,7 +70,7 @@ async fn main() {
             .long("config")
             .value_name("FILE")
             .help("the map configuration file")
-            .takes_value(true)
+            .action(ArgAction::Set)
             .required(true)
         )
         .arg(Arg::new("region")
@@ -78,16 +78,15 @@ async fn main() {
             .long("region")
             .value_name("NAME")
             .help("select a region to render")
-            .takes_value(true)
-            .multiple_occurrences(true)
-            .multiple_values(true)
+            .num_args(1..)
+            .action(ArgAction::Append)
         )
         .arg(Arg::new("listen")
             .short('l')
             .long("listen")
             .value_name("ADDR")
             .help("the addr to listen on")
-            .takes_value(true)
+            .action(ArgAction::Set)
             .default_value("127.0.0.1:8080")
         )
         .arg(Arg::new("style")
@@ -95,17 +94,19 @@ async fn main() {
             .long("style")
             .value_name("STYLE")
             .help("the style to use in the test map")
-            .takes_value(true)
+            .action(ArgAction::Set)
             .default_value("lx")
         )
         .get_matches();
 
-    let config = match Config::load(matches.value_of("config").unwrap()) {
+    let config = match Config::load(
+        matches.get_one::<String>("config").unwrap()
+    ) {
         Ok(config) => config,
         Err(err) => {
             eprintln!(
                 "Failed to load map config {}: {}",
-                matches.value_of("config").unwrap(),
+                matches.get_one::<String>("config").unwrap(),
                 err
             );
             std::process::exit(1)
